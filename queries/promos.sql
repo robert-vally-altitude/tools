e8c1b0af7621cb0e457a83bd1a43d6b3d8722f27
Robert Vally <robert@altitude-games.com>
2025-04-14 13:54:06 +0800

    Add prize value to `promos` query

--- queries/promos.sql
+++ queries/promos.sql
@@ -2,6 +2,7 @@ SELECT
     p.id, 
     p.title, 
     p.status, 
+    pr.price as prize_value,
     p.sweepz_entry as sweepz_per_entry, 
     count(pe.id) as entries, 
     count(distinct pe.playfab_id) as unique_entries, 
@@ -10,6 +11,7 @@ SELECT
     p.end_at
 FROM promo_entries pe
 LEFT JOIN promos p on pe.promo_id = p.id
+LEFT JOIN prizes pr on p.prize_id = pr.startingValue
 WHERE p.start_at >= DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY)
 GROUP BY pe.promo_id
 ORDER BY p.id 
